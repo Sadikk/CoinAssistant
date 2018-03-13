958b15b9fc1cb27fbbd5d7be161fa9fa31b21830
Alicia <Alicia@DESKTOP-NSMA7LT.insa-lyon.fr>
2018-03-13 22:17:51 +0100

    interface graphique - part2

--- CoinAssistant/src/coinAssistant/ui/MainWindow.java
+++ CoinAssistant/src/coinAssistant/ui/MainWindow.java
@@ -1,18 +1,22 @@
 package coinAssistant.ui;
 
 import javax.swing.JFrame;
+import java.awt.GridBagConstraints;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import coinAssistant.core.BinanceConnector;
 
 public class MainWindow extends JFrame {
-	private JPanel conteneur1;
-    private JPanel conteneur2;
-    private JPanel conteneur3;
-    private JPanel conteneur4;
-    private JPanel conteneurPrincipal;
-    private JLabel etiquette1;
+	private JPanel descriptionContainer;
+    private JPanel interpretationContainer;
+    private JPanel trendContainer;
+    private JPanel graphContainer;
+    private JPanel actuatorContainer;
+    private JPanel mainContainer;
+   /* private JLabel etiquette1;
     private JLabel etiquette2;
     private JLabel etiquette3;
     private JLabel etiquette4;
@@ -24,16 +28,58 @@ public class MainWindow extends JFrame {
     private JLabel champs22;
     private JLabel champs31;
     private JLabel champs32;
-    private JLabel champs33;
+    private JLabel champs33;*/
     
 
 	public MainWindow(String nom, int width, int height) {
 	    super(nom);
-		setLayout(null);
 	    setSize(width,height);
 	    setLocation(100,25);
 	    
-	    etiquette1 = new JLabel();
+	    descriptionContainer = new JPanel();
+	    descriptionContainer.setPreferredSize(new Dimension(300, 200)); 
+	    descriptionContainer.setPreferredSize(descriptionContainer.getPreferredSize());
+	    descriptionContainer.setMinimumSize(descriptionContainer.getMinimumSize());
+	    
+	    interpretationContainer = new JPanel();
+	    interpretationContainer.setPreferredSize(new Dimension(300, 200)); 
+	    interpretationContainer.setPreferredSize(interpretationContainer.getPreferredSize());
+	    interpretationContainer.setMinimumSize(interpretationContainer.getMinimumSize());
+	    
+	    trendContainer = new JPanel();
+	    trendContainer.setPreferredSize(new Dimension(300, 200)); 
+	    trendContainer.setPreferredSize( trendContainer.getPreferredSize());
+	    trendContainer.setMinimumSize(trendContainer.getMinimumSize());
+	    
+	    actuatorContainer = new JPanel();
+	    actuatorContainer.setPreferredSize(new Dimension(450, 400)); 
+	    
+	    graphContainer = new JPanel();
+	    graphContainer.setPreferredSize(new Dimension(650, 400)); 
+	  
+	    mainContainer = new JPanel();
+	    mainContainer.setLayout(new GridBagLayout());
+	 
+	    
+	    GridBagConstraints gbc = new GridBagConstraints();
+	    gbc.gridx = gbc.gridy = 0; 
+	    
+	    gbc.fill = GridBagConstraints.BOTH;
+	    gbc.weightx = 1;
+	    gbc.weighty = 1;
+	    
+	    mainContainer.add(descriptionContainer, gbc);
+	    
+	    setContentPane(mainContainer);
+	    
+	    setVisible(true);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	    
+	    
+	    
+	    
+	    /*etiquette1 = new JLabel();
 	    etiquette1.setText("Description du pattern séléctionné");
 	    etiquette1.setBounds(75,10,300,20);
 	    champs11 = new JLabel("-temps auquel le pattern est détecté");        
@@ -45,15 +91,7 @@ public class MainWindow extends JFrame {
 	    champs14 = new JLabel("-caractéristiques de comparaison des différents indicateurs");        
 	    champs14.setBounds(25,125,275,25);
 	    
-	    conteneur1 = new JPanel();
-	    conteneur1.setLayout(null);
-	    conteneur1.add(etiquette1);
-	    conteneur1.add(champs11);
-	    conteneur1.add(champs12);
-	    conteneur1.add(champs13);
-	    conteneur1.add(champs14);
-	    conteneur1.setBounds(15,450,300,220);
-	    
+	
 	    etiquette2 = new JLabel();
 	    etiquette2.setText("Interprétation du pattern");
 	    etiquette2.setBounds(75,10,300,20);
@@ -61,13 +99,6 @@ public class MainWindow extends JFrame {
 	    champs21.setBounds(25,50,275,25);
 	    champs22 = new JLabel("-conclusion sur la tendance soupçonnée");        
 	    champs22.setBounds(25,75,275,25);
-	    
-	    conteneur2 = new JPanel();
-	    conteneur2.setLayout(null);
-	    conteneur2.add(etiquette2);
-	    conteneur2.add(champs21);
-	    conteneur2.add(champs22);
-	    conteneur2.setBounds(400,450,300,220);
 	    
 	    etiquette3 = new JLabel();
 	    etiquette3.setText("Tendance globale");
@@ -79,34 +110,9 @@ public class MainWindow extends JFrame {
 	    champs33 = new JLabel("-conseil à l'achat où à la vente");        
 	    champs33.setBounds(25,100,275,25);
 	    
-	    conteneur3 = new JPanel();
-	    conteneur3.setLayout(null);
-	    conteneur3.add(etiquette3);
-	    conteneur3.add(champs31);
-	    conteneur3.add(champs32);
-	    conteneur3.add(champs33);
-	    conteneur3.setBounds(800,450,300,220);
-	    
 	    etiquette4 = new JLabel();
 	    etiquette4.setText("Actionneurs de l'interface");
-	    etiquette4.setBounds(75,10,300,20);
-	    
-	    conteneur4 = new JPanel();
-	    conteneur4.setLayout(null);
-	    conteneur4.add(etiquette4);
-	    conteneur4.setBounds(800,50,300,220);
-	   
-	    conteneurPrincipal = new JPanel();
-	    conteneurPrincipal.setLayout(null);
-	    conteneurPrincipal.add(conteneur2);
-	    conteneurPrincipal.add(conteneur1);
-	    conteneurPrincipal.add(conteneur3);
-	    conteneurPrincipal.add(conteneur4);
-	    setContentPane(conteneurPrincipal);
-	    
-	    setVisible(true);
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    
+	    etiquette4.setBounds(75,10,300,20);*/
     
     }
 
