762086120317a10803491390554f06f61b6559e9
Sadikk <logs.dofuss@laposte.net>
2018-03-14 09:37:02 +0100

    2 more patterns
    +PiercingLine
    +Twin

--- CoinAssistant/src/coinAssistant/core/candlesticks/PiercingLine.java
+++ CoinAssistant/src/coinAssistant/core/candlesticks/PiercingLine.java
@@ -1,5 +1,25 @@
 package coinAssistant.core.candlesticks;
 
-public class PiercingLine {
+import java.util.ArrayList;
+
+import coinAssistant.core.CandleStick;
+import coinAssistant.core.Pattern;
+
+public class PiercingLine extends Pattern {
+
+	@Override
+	public boolean isPatternPresent(ArrayList<CandleStick> data, int index) {
+		CandleStick first = data.get(index);
+		CandleStick second = data.get(index+1);
+		return !first.isAscend() 
+				&& second.isAscend()
+				&& second.getOpen() < first.getLow()
+				&& second.getClose() > (first.getClose() + first.getBodySize() / 2);
+	}
+
+	@Override
+	public int getPatternSize() {
+		return 2;
+	}
 
 }
--- CoinAssistant/src/coinAssistant/core/candlesticks/Twin.java
+++ CoinAssistant/src/coinAssistant/core/candlesticks/Twin.java
@@ -1,5 +1,39 @@
 package coinAssistant.core.candlesticks;
 
-public class Twin {
+import java.util.ArrayList;
+
+import coinAssistant.core.CandleStick;
+import coinAssistant.core.Pattern;
+
+public class Twin extends Pattern {
+
+	@Override
+	public boolean isPatternPresent(ArrayList<CandleStick> data, int index) {
+		CandleStick first = data.get(index);
+		CandleStick second = data.get(index+1);
+		CandleStick third = data.get(index+2);
+		
+		if (first.isAscend())
+		{
+			return second.getLow() > first.getHigh()
+					&& second.isAscend()
+					&& third.isAscend()
+					&& third.getBodySize() > 0.75 * second.getBodySize()
+					&& third.getBodySize() < 1.25 * second.getBodySize();
+		}
+		else
+		{
+			return second.getHigh() < first.getLow()
+					&& second.isAscend()
+					&& third.isAscend()
+					&& third.getBodySize() > 0.75 * second.getBodySize()
+					&& third.getBodySize() < 1.25 * second.getBodySize();
+		}
+	}
+
+	@Override
+	public int getPatternSize() {
+		return 3;
+	}
 
 }
