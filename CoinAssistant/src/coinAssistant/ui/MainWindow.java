22eb7d8dc5ec72c6e34d3ba9e8cb8322cc9038ce
Alicia <Alicia@DESKTOP-NSMA7LT>
2018-03-14 22:30:42 +0100

    utilisation GridBagLayout dans interface

--- CoinAssistant/src/coinAssistant/ui/MainWindow.java
+++ CoinAssistant/src/coinAssistant/ui/MainWindow.java
@@ -20,19 +20,22 @@ public class MainWindow extends JFrame {
     private JPanel graphContainer;
     private JPanel actuatorContainer;
     private JPanel mainContainer;
-   /* private JLabel etiquette1;
-    private JLabel etiquette2;
-    private JLabel etiquette3;
-    private JLabel etiquette4;
-    private JLabel champs11;
-    private JLabel champs12;
-    private JLabel champs13;
-    private JLabel champs14;
-    private JLabel champs21;
-    private JLabel champs22;
-    private JLabel champs31;
-    private JLabel champs32;
-    private JLabel champs33;*/
+    private JLabel descriptionTitle;
+    private JLabel interpretationTitle;
+    private JLabel trendTitle;
+    private JLabel actuatorTitle;
+    private JLabel graphTitle;
+    private JLabel timeDetection;
+    private JLabel forcePattern;
+    private JLabel lengthPattern;
+    private JLabel comparison;
+    private JLabel explanationInterpretation;
+    private JLabel conclusion;
+    private JLabel sumIndicator;
+    private JLabel globalConclusion;
+    private JLabel advice;
+    private JLabel binarySetting;
+    private JLabel continuousSetting;
     
 
 	public MainWindow(String nom, int width, int height) {
@@ -44,79 +47,122 @@ public class MainWindow extends JFrame {
 	    descriptionContainer.setPreferredSize(new Dimension(300, 200)); 
 	    descriptionContainer.setPreferredSize(descriptionContainer.getPreferredSize());
 	    descriptionContainer.setMinimumSize(descriptionContainer.getMinimumSize());
+	    descriptionTitle = new JLabel("Description du pattern selectionne");
+	    descriptionTitle.setBounds(75,10,300,20);
+	    timeDetection = new JLabel("-temps auquel le pattern est detecte");        
+	    timeDetection.setBounds(25,50,275,25);
+	    forcePattern = new JLabel("-force du pattern");        
+	    forcePattern.setBounds(25,75,275,25);
+	    lengthPattern = new JLabel("-longueur du pattern");        
+	    lengthPattern.setBounds(25,100,275,25);
+	    comparison = new JLabel("-caracteristiques de comparaison des differents indicateurs");        
+	    comparison.setBounds(25,125,275,25);
+	    
+	    descriptionContainer.add(descriptionTitle);
+	    descriptionContainer.add(timeDetection);
+	    descriptionContainer.add(forcePattern);
+	    descriptionContainer.add(lengthPattern);
+	    descriptionContainer.add(comparison);
+	    
 	    
 	    interpretationContainer = new JPanel();
 	    interpretationContainer.setPreferredSize(new Dimension(300, 200)); 
 	    interpretationContainer.setPreferredSize(interpretationContainer.getPreferredSize());
 	    interpretationContainer.setMinimumSize(interpretationContainer.getMinimumSize());
+	    interpretationTitle = new JLabel();
+	    interpretationTitle.setText("Interpretation du pattern");
+	    interpretationTitle.setBounds(75,10,300,20);
+	    explanationInterpretation = new JLabel("-explication de l'interpretation du pattern selectionne ou du dernier pattern");        
+	    explanationInterpretation.setBounds(25,50,275,25);
+	    conclusion = new JLabel("-conclusion sur la tendance soupconnee");        
+	    conclusion.setBounds(25,75,275,25);
+	    
+	    interpretationContainer.add(interpretationTitle);
+	    interpretationContainer.add(explanationInterpretation);
+	    interpretationContainer.add(conclusion);
+	    
 	    
 	    trendContainer = new JPanel();
 	    trendContainer.setPreferredSize(new Dimension(300, 200)); 
 	    trendContainer.setPreferredSize( trendContainer.getPreferredSize());
 	    trendContainer.setMinimumSize(trendContainer.getMinimumSize());
+	    trendTitle = new JLabel();
+	    trendTitle.setText("Tendance globale");
+	    trendTitle.setBounds(75,10,300,20);
+	    sumIndicator = new JLabel("-somme de tous les indicateurs releves");        
+	    sumIndicator.setBounds(25,50,275,25);
+	    globalConclusion = new JLabel("-conclusion sur la tendance globale");        
+	    globalConclusion.setBounds(25,75,275,25);
+	    advice = new JLabel("-conseil a l'achat ou a la vente");        
+	    advice.setBounds(25,100,275,25);
+	    
+	    trendContainer.add(trendTitle);
+	    trendContainer.add(sumIndicator);
+	    trendContainer.add(globalConclusion);
+	    trendContainer.add(advice);
+	    
 	    
 	    actuatorContainer = new JPanel();
-	    actuatorContainer.setPreferredSize(new Dimension(450, 400)); 
+	    actuatorContainer.setPreferredSize(new Dimension(450, 400));
+	    actuatorTitle = new JLabel("Actionneurs de l'interface");
+	    actuatorTitle.setBounds(75,10,300,20);
+	    binarySetting= new JLabel("-reglages binaires : affichage d'elements on/off");        
+	    binarySetting.setBounds(25,50,275,25);
+	    continuousSetting = new JLabel("-reglage continu : taille de l'intervalle de temps considere etc");        
+	    continuousSetting.setBounds(25,75,275,25);
+	    
+	    actuatorContainer.add(actuatorTitle);
+	    actuatorContainer.add(binarySetting);
+	    actuatorContainer.add(continuousSetting);
+	    
 	    
 	    graphContainer = new JPanel();
 	    graphContainer.setPreferredSize(new Dimension(650, 400)); 
+	    graphTitle = new JLabel("Courbe du cours");
+	    graphTitle.setBounds(75,10,300,20);
+	    
+	    graphContainer.add(graphTitle);
 	  
 	    mainContainer = new JPanel();
 	    mainContainer.setLayout(new GridBagLayout());
 	 
 	    
 	    GridBagConstraints gbc = new GridBagConstraints();
-	    gbc.gridx = gbc.gridy = 0; 
+	    gbc.gridx = 0;
+	    gbc.gridy = 0; 
+	    gbc.gridheight = 5;
+	    gbc.gridwidth = 6;
+	    mainContainer.add(graphContainer, gbc);
 	    
-	    gbc.fill = GridBagConstraints.BOTH;
-	    gbc.weightx = 1;
-	    gbc.weighty = 1;
+	    gbc.gridx = 7; 
+	    gbc.gridwidth = GridBagConstraints.REMAINDER;
+	    gbc.gridheight = 5;
+	    gbc.anchor = GridBagConstraints.CENTER;
+	    mainContainer.add(actuatorContainer, gbc);
 	    
+	    gbc.gridx = 0; 
+	    gbc.gridy = 6;
+	    gbc.gridheight = 4;
+	    gbc.gridwidth = 3;
 	    mainContainer.add(descriptionContainer, gbc);
+	    
+	    gbc.gridx = 3; 
+	    gbc.gridy = 6;
+	    gbc.gridheight = 4;
+	    gbc.gridwidth = 3;
+	    mainContainer.add(interpretationContainer, gbc);
+	    
+	    gbc.gridx = 6; 
+	    gbc.gridy = 6;
+	    gbc.gridheight = 4;
+	    gbc.gridwidth = GridBagConstraints.REMAINDER;
+	    mainContainer.add(trendContainer, gbc);
 	    
 	    setContentPane(mainContainer);
 	    
 	    setVisible(true);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    
-	    
-	    
-	    
-	    
-	    /*etiquette1 = new JLabel();
-	    etiquette1.setText("Description du pattern selectionne");
-	    etiquette1.setBounds(75,10,300,20);
-	    champs11 = new JLabel("-temps auquel le pattern est detecte");        
-	    champs11.setBounds(25,50,275,25);
-	    champs12 = new JLabel("-force du pattern");        
-	    champs12.setBounds(25,75,275,25);
-	    champs13 = new JLabel("-longueur du pattern");        
-	    champs13.setBounds(25,100,275,25);
-	    champs14 = new JLabel("-caracteristiques de comparaison des differents indicateurs");        
-	    champs14.setBounds(25,125,275,25);
-	    
-	
-	    etiquette2 = new JLabel();
-	    etiquette2.setText("Interpretation du pattern");
-	    etiquette2.setBounds(75,10,300,20);
-	    champs21 = new JLabel("-explication de l'interpretation du pattern selectionne ou du dernier pattern");        
-	    champs21.setBounds(25,50,275,25);
-	    champs22 = new JLabel("-conclusion sur la tendance soupconnee");        
-	    champs22.setBounds(25,75,275,25);
-	    
-	    etiquette3 = new JLabel();
-	    etiquette3.setText("Tendance globale");
-	    etiquette3.setBounds(75,10,300,20);
-	    champs31 = new JLabel("-somme de tous les indicateurs releves");        
-	    champs31.setBounds(25,50,275,25);
-	    champs32 = new JLabel("-conclusion sur la tendance globale");        
-	    champs32.setBounds(25,75,275,25);
-	    champs33 = new JLabel("-conseil a l'achat ou a la vente");        
-	    champs33.setBounds(25,100,275,25);
-	    
-	    etiquette4 = new JLabel();
-	    etiquette4.setText("Actionneurs de l'interface");
-	    etiquette4.setBounds(75,10,300,20);*/
+	   
     
     }
 
