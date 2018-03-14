afeea2b64e2e2ace73846f49080e2b30ffa48377
Sadikk <logs.dofuss@laposte.net>
2018-03-14 20:55:25 +0100

    2 more patterns
    +BlackSpinningTop
    +WhiteSpinningTop

--- CoinAssistant/src/coinAssistant/core/candlesticks/BlackSpinningTop.java
+++ CoinAssistant/src/coinAssistant/core/candlesticks/BlackSpinningTop.java
@@ -1,5 +1,30 @@
 package coinAssistant.core.candlesticks;
 
-public class BlackSpinningTop {
+import java.util.ArrayList;
+
+import coinAssistant.core.CandleStick;
+import coinAssistant.core.Pattern;
+
+public class BlackSpinningTop extends Pattern {
+	
+	@Override
+	public boolean isPatternPresent(ArrayList<CandleStick> data, int index) {
+		if (index < 2)
+			return false;
+		CandleStick first = data.get(index);
+		CandleStick firstBefore = data.get(index - 1);
+		CandleStick secondBefore = data.get(index - 2); // doit s'inscrire dans une tendance haussière
+		return firstBefore.isAscend() &&
+				secondBefore.isAscend() &&
+				!first.isAscend() &&
+				first.getHigh() > firstBefore.getHigh() &&
+				first.getLowerShadowSize() > 2 * first.getBodySize() &&
+				first.getUpperShadowSize() > 2 * first.getBodySize();
+	}
+
+	@Override
+	public int getPatternSize() {
+		return 1;
+	}
 
 }
--- CoinAssistant/src/coinAssistant/core/candlesticks/WhiteSpinningBottom.java
+++ CoinAssistant/src/coinAssistant/core/candlesticks/WhiteSpinningBottom.java
@@ -1,5 +1,30 @@
 package coinAssistant.core.candlesticks;
 
-public class WhiteSpinningBottom {
+import java.util.ArrayList;
+
+import coinAssistant.core.CandleStick;
+import coinAssistant.core.Pattern;
+
+public class WhiteSpinningBottom extends Pattern {
+
+	@Override
+	public boolean isPatternPresent(ArrayList<CandleStick> data, int index) {
+		if (index < 2)
+			return false;
+		CandleStick first = data.get(index);
+		CandleStick firstBefore = data.get(index - 1);
+		CandleStick secondBefore = data.get(index - 2); // doit s'inscrire dans une tendance baissière
+		return !firstBefore.isAscend() &&
+				!secondBefore.isAscend() &&
+				first.isAscend() &&
+				first.getLow() > firstBefore.getLow() &&
+				first.getLowerShadowSize() > 2 * first.getBodySize() &&
+				first.getUpperShadowSize() > 2 * first.getBodySize();
+	}
+
+	@Override
+	public int getPatternSize() {
+		return 1;
+	}
 
 }
