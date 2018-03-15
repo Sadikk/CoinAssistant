820f2275bcf24f4ec1c841405865768702543dda
Alicia <Alicia@10.43.2.80>
2018-03-15 09:52:13 +0100

    engulfing pattern méthode

--- CoinAssistant/src/coinAssistant/core/candlesticks/Engulfing.java
+++ CoinAssistant/src/coinAssistant/core/candlesticks/Engulfing.java
@@ -1,5 +1,31 @@
 package coinAssistant.core.candlesticks;
 
-public class Engulfing {
+import java.util.ArrayList;
+
+import coinAssistant.core.CandleStick;
+import coinAssistant.core.Pattern;
+
+public class Engulfing extends Pattern {
+	
+	@Override
+	public boolean isPatternPresent(ArrayList<CandleStick> data, int index) {
+		CandleStick first = data.get(index);
+		CandleStick second = data.get(index-1);
+		if (first.getClose()>second.getHigh() && first.getOpen()<second.getLow())
+		{
+			return first.isAscend()
+					&& !second.isAscend();
+		}
+		else if (first.getClose()<second.getLow() && first.getOpen()>second.getHigh())
+		{
+			return !first.isAscend() 
+					&& second.isAscend();
+		}
+		return false;
+	}
+	@Override
+	public int getPatternSize() {
+		return 1;
+	}
 
 }
