5dc622fb6b3b709db6fc9b1d1fd50f21770e3e2b
Arthur <Arthur@10.43.6.131>
2018-03-15 09:01:28 +0100

    1 nouveau pattern

--- CoinAssistant/src/coinAssistant/core/candlesticks/UpsideTasukiGap.java
+++ CoinAssistant/src/coinAssistant/core/candlesticks/UpsideTasukiGap.java
@@ -1,5 +1,29 @@
 package coinAssistant.core.candlesticks;
 
-public class UpsideTasukiGap {
+import java.util.ArrayList;
+import coinAssistant.core.*;
 
+public class UpsideTasukiGap extends Pattern{
+	static private int taillePattern=3;
+	public UpsideTasukiGap() {}
+	@Override
+	public boolean isPatternPresent(ArrayList<CandleStick> data, int rg) {
+		CandleStick first=data.get(rg);
+		CandleStick second=data.get(rg+1);
+		CandleStick third=data.get(rg+2);
+		
+		return (first.isAscend() //first up
+		&& first.getClose()<second.getOpen() //gap up
+		&& second.isAscend()// second up
+		&& !third.isAscend() //third down
+		&& third.getOpen()>second.getOpen() //third opens in body of second
+		&& third.getClose()<second.getOpen() //third closes in the gap up
+		&& third.getClose()>first.getClose());
+		
+	}
+	
+	@Override
+	public int getPatternSize() {return taillePattern;}
 }
+
+//source : https://hitandruncandlesticks.com/downside-tasuki-gap/
